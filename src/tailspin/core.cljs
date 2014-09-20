3ae156eaf01c778bf200cfec4d85d8c8caa44613
Max Kreminski <maxkreminski@gmail.com>
2014-09-20 12:17:11 -0700

    calculate initial cell :output on will-mount

--- src/tailspin/core.cljs
+++ src/tailspin/core.cljs
@@ -9,29 +9,30 @@
 (def app-state
   (atom {:cells [{:type :code
                   :name "testing"
-                  :input "(str \"Hello, \" \"world!\")"
-                  :output {:value "Hello, world!"}}]}))
+                  :input "(str \"Hello, \" \"world!\")"}]}))
 
-(defn- simple-eval [form]
-  (let [smap {'+ + '- - '* * '/ / 'apply apply 'str str}
-        form' (walk/prewalk-replace smap form)]
-    (apply (first form') (rest form'))))
+(defn- shitty-eval [code]
+  (let [smap {'+ + '- - '* * '/ / 'apply apply 'str str}]
+    (try (let [form (->> (rdr/read-string code) (walk/prewalk-replace smap))]
+           {:value (apply (first form) (rest form))})
+         (catch js/Error err
+           {:error (.-message err)}))))
 
 (defn- handle-change [cell ev]
   (let [input (.. ev -target -value)]
-    (om/update! cell :input input)
-    (try (let [form (rdr/read-string input)
-               result (simple-eval form)]
-           (om/update! cell :output {:value result}))
-         (catch js/Error err
-           (om/update! cell :output {:error (.-message err)})))))
+    (om/transact! cell []
+      #(merge % {:input input :output (shitty-eval input)}))))
 
 (defn- render-result [{:keys [error value]}]
   (dom/div #js {:className (str "output " (if error "failure" "success"))}
     (str "==> " (or error (pr-str value)))))
 
 (defn code-cell [cell owner]
-  (reify om/IRender
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (om/update! cell :output (shitty-eval (:input cell))))
+    om/IRender
     (render [_]
       (dom/div #js {:className "cell code"}
         (dom/input
