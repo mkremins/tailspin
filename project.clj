31dbfdb9b74b4d6a69c3eddcc0687b63e27efc97
Max Kreminski <maxkreminski@gmail.com>
2014-09-20 10:31:42 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject mkremins/tailspin "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2322"]
+                 [mkremins/dep-graph "0.1.0"]
+                 [mkremins/flense "0.0-SNAPSHOT"]
+                 [om "0.7.1"]])
